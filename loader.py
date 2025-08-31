edd19e317a39e52a8a2ad79fe3f6504b92f73143
GuilhermeMP1980 <108761568+GuilhermeMP1980@users.noreply.github.com>
2025-08-31 20:25:10 -0300

    Update loader.py

--- loader.py
+++ loader.py
@@ -4,12 +4,13 @@ import os
 def load_files(folder_path):
     dfs = []
     for file in os.listdir(folder_path):
-        if file.endswith("C:\\Users\\Manoel\\Documents\\DNC\\Exercícios Analista de Dados\\Desafios Analista de Dados\\data.csv"):
+        if file.endswith("data.csv"):
             df = pd.read_csv(os.path.join(folder_path, file))
-        elif file.endswith("C:\\Users\\Manoel\\Documents\\DNC\\Exercícios Analista de Dados\\Desafios Analista de Dados\\Desafio PBI.xlsx"):
+        elif file.endswith("Data.xlsx"):
             df = pd.read_excel(os.path.join(folder_path, file))
         else:
             continue
         df.columns = df.columns.str.strip().str.lower()
         dfs.append(df)
+
     return dfs
