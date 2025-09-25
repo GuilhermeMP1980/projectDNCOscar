617c2dbdb6dcef105f9d44c8de3e01ab89346957
GuilhermeMP1980 <108761568+GuilhermeMP1980@users.noreply.github.com>
2025-09-25 16:47:11 -0300

    Create security.py

---
+++ security.py
@@ -0,0 +1,16 @@
+# security.py
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+
+SECRET_KEY = "sua-chave-secreta"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
