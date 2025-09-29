e83d50ecfb177e5411ebe23be79bcec30a747c11
GuilhermeMP1980 <108761568+GuilhermeMP1980@users.noreply.github.com>
2025-09-29 16:56:39 -0300

    Create streamlitapp.py

---
+++ testes/streamlitapp.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="Dashboard de Estoque", layout="wide")
+st.title("Dashboard Inteligente de Estoque - Empresa de Calçados")
+
+st.markdown("""
+Bem-vindo ao painel interativo de análise de estoque.  
+Escolha uma das páginas à esquerda para explorar os dados de **inventário**, **devoluções**, **cancelamentos** ou **ajustes de estoque**.
+""")
